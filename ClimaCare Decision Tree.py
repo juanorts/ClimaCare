465878f6390d9f4b8dbfe26fb66b55c453d7a81d
Juan Orts <91051695+juanorts@users.noreply.github.com>
2024-01-08 19:41:30 +0100

    Final code fixes

--- ClimaCare Decision Tree.py
+++ ClimaCare Decision Tree.py
@@ -1,10 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
+'''
 
-# ## Imports
+This code provides the machine learning algorithms to make day profile predictions based
+on the weather condition parameters provided at a specific datetime for the ClimaCare
+project. It uses Decision TreeClassifier from the Scikit-Learn library to classify the
+weather conditions and relate them to a type of weather (day profile).
 
-# In[1]:
+'''
 
+# Imports
 
 import pandas as pd
 import pandas as pd
@@ -14,127 +17,42 @@ from sklearn import metrics #Import scikit-learn metrics module for accuracy cal
 from sklearn.preprocessing import LabelEncoder
 
 
-# In[2]:
+# Read the training dataset
 
+df = pd.read_csv("data/BilbaoWeatherDataset.csv", sep = ";")
 
-df = pd.read_csv("BilbaoWeatherDataset.csv", sep = ";")
+# Data processing
 
-
-# In[3]:
-
-
-df.head()
-
-
-# ### Quitar guiones adicionales de los labels
-
-# In[4]:
-
-
+# Remove ending hypens in the labels
 df['DAY-PROFILE 1'] = df['DAY-PROFILE 1'].apply(lambda x: x[:-3] if x.endswith(" - ") else x)
 df['DAY-PROFILE 2'] = df['DAY-PROFILE 2'].apply(lambda x: x[:-3] if x.endswith(" - ") else x)
 
-
-# In[5]:
-
-
-df.head()
-
-
-# In[23]:
-
-
-df['DAY-PROFILE 2'].value_counts()
-
-
-# In[7]:
-
-
-df['DAY-PROFILE 2'].unique()
-
-
-# ### Extraer mes de la fecha
-
-# In[8]:
-
-
 # Convert the 'date' column to a datetime type
-df['DATE'] = pd.to_datetime(df['DATE'])
+df['DATE'] = pd.to_datetime(df['DATE'], format = '%d/%m/%Y')
 
-# Extract the month and create a new column 'month'
+# Extract the month and create a new column 'month' to be used as a feature variable
 df['MONTH'] = df['DATE'].dt.month
 
-
-# In[9]:
-
-
-df.head()
-
-
-# ## Preparación de los datos para training y test
-
-# ### Datos de entrenamiento
-
-# In[10]:
-
+# Training sets
 
 X1_train = df[['TEMPERATURE', 'HUMIDITY', 'WINDSPEED', 'MONTH']] # Features
 y1_train = df['DAY-PROFILE 1'] # Target variable
 
-
-# In[11]:
-
-
 X2_train = df[['PRESSURE', 'UV INDEX', 'AIR QUALITY', 'MONTH']] # Features
 y2_train = df['DAY-PROFILE 2'] # Target variable
 
-
-# In[12]:
-
-
-# Split the first dataset (X1, y1)
-#X1_train, X1_test, y1_train, y1_test = train_test_split(X1, y1, test_size=0.2, random_state=1)
-
-# Split the second dataset (X2, y2) using the same indices as the first split
-#X2_train, X2_test, y2_train, y2_test = train_test_split(X2, y2, test_size=0.2, random_state=1) 
-# Note: Using 'stratify=y1' ensures that the same stratification is applied as in the first split
-
-
-# ### Dato a evaluar
-
-# In[13]:
-
+# Create dataframes for the collected weather conditions
 
 X1_test = pd.DataFrame(data=[[32, 70, 7, 8]], columns=['TEMPERATURE', 'HUMIDITY', 'WINDSPEED', 'MONTH'])
 
 X2_test = pd.DataFrame([[1000, 4, 54, 8]], columns=['PRESSURE', 'UV INDEX', 'AIR QUALITY', 'MONTH'])
 
-
-# In[14]:
-
-
-X1_test
-
-
-# In[15]:
-
-
-X2_test
-
-
-# ## Clasificación usando Decision Trees
-
-# In[16]:
-
+# Decision Tree Classification
 
 # Create Decision Tree classifer object
 clf = DecisionTreeClassifier()
 
-
-# ### Decision Tree para el primer perfil de día
-
-# In[17]:
-
+# First day profile: temp, humidity, wind speed and month
 
 # Train Decision Tree Classifer
 clf1 = clf.fit(X1_train, y1_train)
@@ -142,37 +60,15 @@ clf1 = clf.fit(X1_train, y1_train)
 #Predict the response for test dataset
 y1_pred = clf1.predict(X1_test)
 
-
-# In[18]:
-
-
-# Model Accuracy, how often is the classifier correct?
-#print("Accuracy:", metrics.accuracy_score(y1_test, y1_pred))
-
-
-# ### Decision Tree para el segundo perfil de día
-
-# In[19]:
-
+# Second day profile: atmospheric pressure, uv index, air quality index, month
 
 # Train Decision Tree Classifer
 clf2 = clf.fit(X2_train, y2_train)
 
-#Predict the response for test dataset
+# Predict the response for test dataset
 y2_pred = clf2.predict(X2_test)
 
-
-# In[20]:
-
-
-# Model Accuracy, how often is the classifier correct?
-#print("Accuracy:", metrics.accuracy_score(y2_test, y2_pred))
-
-
-# ### Mostrar los resultados de predicción
-
-# In[21]:
-
+# Classification results
 
 result_df1 = pd.DataFrame({'Predicted 1': y1_pred})
 result_df2 = pd.DataFrame({'Predicted 2': y2_pred})
@@ -182,9 +78,5 @@ test_df2 = pd.DataFrame(X2_test, columns=['PRESSURE', 'UV INDEX', 'AIR QUALITY',
 test_df = pd.concat([test_df1, test_df2], axis=1)
 result_df = pd.concat([test_df, result_df], axis=1)
 
-
-# In[22]:
-
-
-result_df.head(60)
-
+pd.set_option('display.max_columns', None)
+print(result_df['TEMPERATURE'].values[0])
--- dataCollectionProcessing.py
+++ dataCollectionProcessing.py
@@ -2,7 +2,7 @@
 
 This code is used to obtain data from the sensors for the ClimaCare project. The collected data is processed
 and classified into a day profile using Decision Tree Classifiers. The processed data will be stored in the
-database.
+InfluxDB server and displayed in a Grafana dashboard.
 
 SENSORS:
 - Temperature & Humidity Sensor (Port D5)
@@ -20,6 +20,15 @@ from seeed_dht import DHT # Temp & humidity sensor
 import requests # REST APIs
 import subprocess
 import datetime
+import pandas as pd
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
+from sklearn.model_selection import train_test_split # Import train_test_split function
+from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
+from sklearn.preprocessing import LabelEncoder
+import os
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 # Temperature and humidity
 def getTemperatureHumidity():
@@ -229,16 +238,17 @@ def read_pressure():
 # Main
 
 if __name__ == "__main__":
-    
-    # Run this command for the Dust Sensor
-    import subprocess
-    command = "sudo pigpiod"
-    process = subprocess.Popen(command, shell=True)
-    process.wait()
-    if process.returncode == 0:
-        print("Command executed successfully")
-    else:
-        print("Error executing command")
+    # Connect to InfluxDB
+
+    token = "NLg-1hN2eYHYPnuejFJpKWzXovUfteDWqTgeetClKGNUSgbOoqa5oOyFtw4ThisiZBlDkKHjTnz_Ld3ERLxo3A=="
+    org = "ClimaCare"
+    url = "http://127.0.0.1:8086"
+
+    client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+
+    bucket="climacare-db"
+
+    write_api = client.write_api(write_options=SYNCHRONOUS)
     
     ''' For each parameter we will obtain the value every minute and calculate the average value every 30 minutes '''
     while True:
@@ -268,18 +278,12 @@ if __name__ == "__main__":
             while temp == 0 and humidity == 0:
                 temp, humidity = getTemperatureHumidity()
             sumTemp += temp
-            sumHumidity += humidity
-            
-            print("Temperature:", temp, "ºC")
-            print("Humidity:", humidity, "%")    
+            sumHumidity += humidity   
             
             # Wind speed and UV Index
             windsp, uv =  getWindSpeedUVIndex()
             sumWindSpeed += windsp
             sumUV += uv
-            
-            print("Wind speed:", windsp)
-            print("UV Index:", uv)
                        
             # Pollen concentrations
             alder_pollen, birch_pollen, grass_pollen, mugwort_pollen, olive_pollen, ragweed_pollen = getPollenConcentrations()
@@ -289,17 +293,10 @@ if __name__ == "__main__":
             sumMugwortPollen += mugwort_pollen
             sumOlivePollen += olive_pollen
             sumRagweedPollen += ragweed_pollen
-            
-            print("Pollen concentrations:", alder_pollen, birch_pollen, grass_pollen, mugwort_pollen, olive_pollen, ragweed_pollen)
 
             # Pressure
             pressure_value = read_pressure()
             sumPressure += pressure_value
-
-            if pressure_value is not None:
-                print(f"Pressure: {pressure_value} hPa")
-            else:
-                print("Failed to read pressure.")
             
             # Air quality
             pi = pigpio.pi()  # Connect to Pi
@@ -308,12 +305,15 @@ if __name__ == "__main__":
             aqi = dustsensor.get_aqi_and_pm_values()
             sumAQI += float(aqi)
             
-            print("Air Quality:", aqi)
-            
             countMin += 1 # Increment the minute count
             time.sleep(30) # Wait for the resting 30 seconds in a minute
         
-        # If the number of minutes passed is 30 then calculate the mean values and save them in the DB
+        '''
+
+        If 30 minutes passed calculate the mean value for each parameter and classify the data into a day parameter
+        using a Decision Tree Classifier. Then, write the data into InfluxDB.
+
+        '''
         if (countMin == 30):
             # Temperature
             resultTemp = sumTemp / 30
@@ -337,33 +337,141 @@ if __name__ == "__main__":
             
             # Air quality           
             resultAQI = sumAQI / 30
-            
-            aqi_category = ""
-
-            if 0 <= int(resultAQI) <= 50:
-                aqi_category = "Good"
-            elif 51 <= int(resultAQI) <= 100:
-                aqi_category = "Moderate"
-            elif 101 <= int(resultAQI) <= 150:
-                aqi_category = "Unhealthy for sensitive groups"
-            elif 151 <= int(resultAQI) <= 200:
-                aqi_category = "Unhealthy"
-            elif 201 <= int(resultAQI) <= 300:
-                aqi_category = "Very unhealthy"
-            else:
-                aqi_category = "Hazardous"
                 
             # Pressure
             resultPressure = sumPressure / 30
             
-            # Print results
-            print("\nDATA FROM", datetime.datetime.now())
-            print("Temperature:", resultTemp, "ºC")
-            print("Humidity:", resultHumidity, "%")
-            print("Wind Speed:", resultWS, "km/h")
-            print("UV:", resultUV)
-            print("Pollen concentrations:", resultAP, resultBP, resultGP, resultMP, resultOP, resultRP)
-            print("Air Quality:", resultAQI, aqi_category)
-            print("Pressure:", resultPressure)
-            print("_________________________\n")
+            '''
             
+            Decision Tree Classification
+            
+            '''
+            
+            # Data classification (DECISION TREE CLASSIFICATION)
+            
+            # Read the training dataset
+
+            df = pd.read_csv("data/BilbaoWeatherDataset.csv", sep = ";")
+
+            # Data processing
+
+            # Remove ending hypens in the labels
+            df['DAY-PROFILE 1'] = df['DAY-PROFILE 1'].apply(lambda x: x[:-3] if x.endswith(" - ") else x)
+            df['DAY-PROFILE 2'] = df['DAY-PROFILE 2'].apply(lambda x: x[:-3] if x.endswith(" - ") else x)
+
+            # Convert the 'date' column to a datetime type
+            df['DATE'] = pd.to_datetime(df['DATE'], format = '%d/%m/%Y')
+
+            # Extract the month and create a new column 'month' to be used as a feature variable
+            df['MONTH'] = df['DATE'].dt.month
+
+            # Training sets
+
+            X1_train = df[['TEMPERATURE', 'HUMIDITY', 'WINDSPEED', 'MONTH']] # Features
+            y1_train = df['DAY-PROFILE 1'] # Target variable
+
+            X2_train = df[['PRESSURE', 'UV INDEX', 'AIR QUALITY', 'MONTH']] # Features
+            y2_train = df['DAY-PROFILE 2'] # Target variable
+
+            # Create dataframes for the collected weather conditions
+
+            X1_test = pd.DataFrame(data=[[resultTemp, resultHumidity, resultWS, datetime.datetime.now().month]], columns=['TEMPERATURE', 'HUMIDITY', 'WINDSPEED', 'MONTH'])
+
+            X2_test = pd.DataFrame([[resultPressure, resultUV, resultAQI, datetime.datetime.now().month]], columns=['PRESSURE', 'UV INDEX', 'AIR QUALITY', 'MONTH'])
+
+            # Decision Tree Classification
+
+            # Create Decision Tree classifer object
+            clf = DecisionTreeClassifier()
+
+            # First day profile: temp, humidity, wind speed and month
+
+            # Train Decision Tree Classifer
+            clf1 = clf.fit(X1_train, y1_train)
+
+            #Predict the response for test dataset
+            y1_pred = clf1.predict(X1_test)
+
+            # Second day profile: atmospheric pressure, uv index, air quality index, month
+
+            # Train Decision Tree Classifer
+            clf2 = clf.fit(X2_train, y2_train)
+
+            # Predict the response for test dataset
+            y2_pred = clf2.predict(X2_test)
+
+            # Classification results
+
+            result_df1 = pd.DataFrame({'Predicted 1': y1_pred})
+            result_df2 = pd.DataFrame({'Predicted 2': y2_pred})
+            result_df = pd.concat([result_df1, result_df2], axis=1)
+            test_df1 = pd.DataFrame(X1_test, columns=['TEMPERATURE', 'HUMIDITY', 'WINDSPEED', 'MONTH'])
+            test_df2 = pd.DataFrame(X2_test, columns=['PRESSURE', 'UV INDEX', 'AIR QUALITY', 'MONTH'])
+            test_df = pd.concat([test_df1, test_df2], axis=1)
+            result_df = pd.concat([test_df, result_df], axis=1)
+            
+            '''
+
+            Recommendations for predicted day profiles
+            
+            '''
+            
+            recommendationStr = []
+            
+            # Day profile 1
+            if(result_df['Predicted 1'].values[0] == "Cold"):
+                recommendationStr = ["Llevar una chaqueta aislante de alta calidad para mantenerse abrigado", "Vestir con una capa intermedia entre la camisa y el abrigo", "Cubrir el cuello y cabeza, y usar guantes para las manos"]
+            elif("Cold - High Humidity" in result_df['Predicted 1'].values[0]):
+                recommendationStr = ["Vestir con una capa con materiales transpirables", "Llevar una capa exterior resistente al agua", "Usar calzado cálido e impermeable"]
+            elif(result_df['Predicted 1'].values[0] == "Hot"):
+                recommendationStr = ["Optar por comidas ligeras y ricas en agua, como vegetales y frutas, y evitar platos pesados", "Beber uno o dos litros de agua al día y evitar el alcohol", "Mantener algunas partes de tu cuerpo frescas, como los pies, tobillos, muñecas, la nuca, antebrazos y la sien", "Vestir ropa ligera y de colores claros"]
+            elif("Hot - High Humidity" in result_df['Predicted 1'].values[0]):
+                recommendationStr = ["Optar por comidas ligeras y ricas en agua, como vegetales y frutas, y evitar platos pesados", "Beber uno o dos litros de agua al día y evitar el alcohol", "Mantener algunas partes de tu cuerpo frescas, como los pies, tobillos, muñecas, la nuca, antebrazos y la sien", "Vestir ropa ligera y de colores claros", "A la hora de hacer ejercicio optar por las partes más frescas del día, temprano en la mañana o al atardecer"]
+            elif(result_df['Predicted 1'].values[0] == "High Humidity"):
+                recommendationStr = ["Vestir con una capa con materiales transpirables", "Es crucial mantener una hidratación constante", "A la hora de realizar actividad física optar por áreas bien ventiladas"]
+
+            if("Windy" in result_df['Predicted 1'].values[0]):
+                recommendationStr = recommendationStr + ["Usar chaquetas y pantalones fabricados con materiales diseñados para bloquear el viento", "Proteger los ojos con gafas de sol o gafas protectoras"]
+            elif("Strong Wind" in result_df['Predicted 1'].values[0]):
+                recommendationStr = recommendationStr + ["Usar chaquetas y pantalones fabricados con materiales diseñados para bloquear el viento", "En la calle, mantenterse alejado de cornisas, balcones y evitar áreas con sitios en construcción", "Evitar viajar en motocicleta o bicicleta;Proteger los ojos con gafas de sol o gafas protectoras"]
+
+            # Day profile 2
+            if("High UV" in result_df['Predicted 2'].values[0]):
+                recommendationStr = recommendationStr + ["Al mediodía, mantenerse a la sombra", "Vestir ropa adecuada, un sombrero y gafas de sol", "Usar suficiente protector solar con la protección adecuada para la piel"]
+            if("Extreme UV" in result_df['Predicted 2'].values[0]):
+                recommendationStr = recommendationStr + ["Tomar precauciones adicionales, la piel no protegida puede dañarse y quemarse rápidamente", "Mantenterse alejado de reflectores de rayos UV como la arena blanca o superficies brillantes", "Usar suficiente protector solar con la protección adecuada para la piel", "Evitar el sol entre las 11:00 y las 16:00"]
+            if("Low Pressure" in result_df['Predicted 2'].values[0]):
+                recommendationStr = recommendationStr + ["Llevar un paraguas resistente al viento para protegerte de la lluvia", "Extremar las precauciones al conducir, ya que puede haber tormentas", "Si se es sensible a los cambios en la presión atmosférica, tomar precauciones adicionales, llevar los medicamentos necesarios y mantenerse hidratado"]
+            if("Moderate AQI" in result_df['Predicted 2'].values[0]):
+                recommendationStr = recommendationStr + ["Es seguro participar en actividades al aire libre, pero las personas extremadamente sensibles a la calidad del aire pueden considerar reducir la intensidad y duración de dichas actividades", "Si perteneces a un grupo sensible (como niños pequeños, personas mayores o aquellos con problemas respiratorios o cardíacos), puedes considerar limitar tu tiempo al aire libre"]
+            if("Unhealthy AQI" in result_df['Predicted 2'].values[0]):
+                recommendationStr = recommendationStr + ["Si se experimentan síntomas como irritación ocular, irritación de garganta, dificultad para respirar o problemas respiratorios, considerar reducir las actividades al aire libre", "El uso de mascarillas protectoras puede ser considerado, especialmente para aquellos sensibles a la calidad del aire"]
+
+            '''
+            
+            Add pollen alerts
+
+            '''
+            pollenLevel = ""
+
+            if((40 < resultAP < 80) or (40 < resultBP < 80) or (10 < resultGP < 50) or (20 < resultMP < 30) or (50 < resultOP < 200) or (10 < resultRP < 50)):
+                pollenLevel = "Medio"
+            if((resultAP > 80) or (resultBP > 80) or (resultGP > 50) or (resultMP > 30) or (resultOP > 200) or (resultRP > 50)):
+                pollenLevel = "Alto"
+            if(pollenLevel == ""):
+                pollenLevel = "Bajo"          
+            
+            '''
+            
+            InfluxDB: write data into bucket
+            
+            '''
+            
+            # Write data into InfluxDB bucket
+            influxdata = influxdb_client.Point("measure").tag("location", "Universidad de Deusto").field("temperture", result_df['TEMPERATURE'].values[0]).field("humidity", result_df['HUMIDITY'].values[0]).field("windspeed", result_df['WINDSPEED'].values[0]).field("pressure", result_df['PRESSURE'].values[0]).field("uv", result_df['UV INDEX'].values[0]).field("air_quality", result_df['AIR QUALITY'].values[0]).field("pollen", pollenLevel)
+            write_api.write(bucket=bucket, org=org, record=influxdata)
+            for r in recommendationStr:
+                influxdata = influxdb_client.Point("measure").tag("location", "Universidad de Deusto").field("recommendations", r)
+                write_api.write(bucket=bucket, org=org, record=influxdata)
+                
+            print("Data written succesfully")
